db7a945a26afc2cebf35a57330ad491765bea379
Alexander Schulze <cibernop@gmail.com>
2016-02-15 09:13:46 +0100

    Started features
    Added feature files to handle the function calls
    Began ‘start’ function

---
+++ features.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Alexander Schulze on 15.02.16.
+//
+
+#include "features.h"
+
+#include <chrono>
+
+
+namespace features
+{
+    void start()
+    {
+        // Get current time
+
+        // Calculate end time
+
+        // Print start and approx end time
+    }
+}
+
---
+++ features.h
@@ -0,0 +1,13 @@
+//
+// Created by Alexander Schulze on 15.02.16.
+//
+
+#ifndef TIME_TRACKER_FEATURES_H
+#define TIME_TRACKER_FEATURES_H
+
+namespace features
+{
+    void start();
+}
+
+#endif //TIME_TRACKER_FEATURES_H
